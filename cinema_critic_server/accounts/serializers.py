0169f96076ff30ab5961a1c2db00a6d758986096
Tsvetan Eftenov <105812168+eftenow@users.noreply.github.com>
2023-07-16 18:44:28 +0300

    login/register/logout functionality is created

--- cinema_critic_server/accounts/serializers.py
+++ cinema_critic_server/accounts/serializers.py
@@ -1,66 +1,21 @@
-from django.contrib.auth import get_user_model, password_validation, authenticate
-from django.core import exceptions
+from django.contrib.auth import get_user_model
 from rest_framework import serializers
 
 UserModel = get_user_model()
 
 
 class RegisterUserSerializer(serializers.ModelSerializer):
-    repeat_password = serializers.CharField(write_only=True)
-
     class Meta:
         model = UserModel
-        fields = ('username', 'email', 'password', 'repeat_password')
+        fields = ['id', 'name', 'email', 'password']
+        extra_kwargs = {
+            'password': {'write_only': True}
+        }
 
     def create(self, validated_data):
-        validated_data.pop('repeat_password', None)
-        user = super().create(validated_data)
-
-        password = validated_data.get('password')
-        user.set_password(password)
-        user.save()
-
-        return user
-
-    def to_representation(self, instance):
-        return {}
-
-    def validate(self, data):
-        password = data.get('password')
-        repeat_password = data.get('repeat_password')
-
-        if password and repeat_password and password != repeat_password:
-            raise serializers.ValidationError("Passwords do not match.")
-
-        user = UserModel(**data)
-
-        errors = dict()
-        try:
-            password_validation.validate_password(password=password, user=user)
-
-        except exceptions.ValidationError as e:
-            errors['password'] = list(e.messages)
-
-        if errors:
-            raise serializers.ValidationError(errors)
-
-        return super().validate(data)
-
-
-class LoginUserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = UserModel
-        fields = ('username', 'password')
-
-    def validate(self, data):
-        username = data.get('username')
-        password = data.get('password')
-
-        if not username or not password:
-            raise serializers.ValidationError("Username and password are required.")
-
-        user = authenticate(username=username, password=password)
-        if not user:
-            raise serializers.ValidationError("Invalid credentials. Please try again.")
-
-        return data
+        password = validated_data.pop('password', None)
+        instance = self.Meta.model(**validated_data)
+        if password is not None:
+            instance.set_password(password)
+        instance.save()
+        return instance
--- cinema_critic_server/accounts/views.py
+++ cinema_critic_server/accounts/views.py
@@ -1,34 +1,78 @@
 from django.contrib.auth import get_user_model
-from rest_framework import generics
-from rest_framework.authtoken.views import APIView
+from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework.exceptions import AuthenticationFailed
 
-from cinema_critic_server.accounts.serializers import RegisterUserSerializer, LoginUserSerializer
+
+import jwt, datetime
+
+from cinema_critic_server.accounts.serializers import RegisterUserSerializer
 
 UserModel = get_user_model()
 
 
-class RegisterUserView(generics.CreateAPIView):
-    queryset = UserModel.objects.all()
-    serializer_class = RegisterUserSerializer
+# Create your views here.
+class RegisterUserView(APIView):
+    def post(self, request):
+        serializer = RegisterUserSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
 
 
-class LoginUserView(TokenObtainPairView):
-    serializer_class = LoginUserSerializer
+class LoginUserView(APIView):
+    def post(self, request):
+        username = request.data['username']
+        password = request.data['password']
+
+        user = UserModel.objects.filter(username=username).first()
+
+        if user is None:
+            raise AuthenticationFailed('User not found!')
+
+        if not user.check_password(password):
+            raise AuthenticationFailed('Incorrect password!')
+
+        payload = {
+            'id': user.id,
+            'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=60),
+            'iat': datetime.datetime.utcnow()
+        }
+
+        token = jwt.encode(payload, 'secret', algorithm='HS256').decode('utf-8')
+
+        response = Response()
+
+        response.set_cookie(key='jwt', value=token, httponly=True)
+        response.data = {
+            'jwt': token
+        }
+        return response
 
 
 class LogoutUserView(APIView):
+
     def get(self, request):
-        self.__perform_logout(request)
+        token = request.COOKIES.get('jwt')
 
+        if not token:
+            raise AuthenticationFailed('Unauthenticated!')
+
+        try:
+            payload = jwt.decode(token, 'secret', algorithm=['HS256'])
+        except jwt.ExpiredSignatureError:
+            raise AuthenticationFailed('Unauthenticated!')
+
+        user = UserModel.objects.filter(id=payload['id']).first()
+        serializer = RegisterUserSerializer(user)
+        return Response(serializer.data)
+
+
+class LogoutView(APIView):
     def post(self, request):
-        self.__perform_logout(request)
-
-    @staticmethod
-    def __perform_logout(request):
-        print(request.user)
-        request.user.auth_token.delete()
-        return Response(
-            {'message': 'user logged out'}
-        )
+        response = Response()
+        response.delete_cookie('jwt')
+        response.data = {
+            'message': 'success'
+        }
+        return response
