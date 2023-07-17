faf0d695da7797cef10dd0fc49940a59b3d07ef8
Victoria Stoyanova <your_github_email>
2023-07-17 19:25:20 +0300

    created movie reviews and series reviews as two separate models

--- cinema_critic_server/reviews/models.py
+++ cinema_critic_server/reviews/models.py
@@ -5,16 +5,27 @@ from cinema_critic_server.movies.models import Movie
 from cinema_critic_server.series.models import Series
 
 UserModel = get_user_model()
-class Review(models.Model):
+
+
+class MovieReview(models.Model):
     review_title = models.CharField(max_length=30)
-    review_description = models.TextField(
+    user = models.ForeignKey(UserModel, on_delete=models.CASCADE)
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    content = models.TextField(
         blank=True,
-        null=True,
+        null=True
     )
-    user = models.ForeignKey(
-        UserModel,
-        on_delete=models.CASCADE)
-    movie = models.ForeignKey(Movie, on_delete=models.CASCADE, null=True, blank=True)
-    series = models.ForeignKey(Series, on_delete=models.CASCADE, null=True, blank=True)
-    content = models.TextField()
+    rating = models.IntegerField()
+
+    # Add other fields for the movie review as needed
+
+
+class SeriesReview(models.Model):
+    review_title = models.CharField(max_length=30)
+    user = models.ForeignKey(UserModel, on_delete=models.CASCADE)
+    series = models.ForeignKey(Series, on_delete=models.CASCADE)
+    content = models.TextField(
+        blank=True,
+        null=True
+    )
     rating = models.IntegerField()
