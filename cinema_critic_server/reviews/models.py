100d0f9ea347fce6b51b19a0054b3c06e41f88c0
Victoria Stoyanova <your_github_email>
2023-07-17 15:50:35 +0300

    created Review model

--- cinema_critic_server/reviews/models.py
+++ cinema_critic_server/reviews/models.py
@@ -1,3 +1,20 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+from cinema_critic_server.movies.models import Movie
+from cinema_critic_server.series.models import Series
+
+UserModel = get_user_model()
+class Review(models.Model):
+    review_title = models.CharField(max_length=30)
+    review_description = models.TextField(
+        blank=True,
+        null=True,
+    )
+    user = models.ForeignKey(
+        UserModel,
+        on_delete=models.CASCADE)
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE, null=True, blank=True)
+    series = models.ForeignKey(Series, on_delete=models.CASCADE, null=True, blank=True)
+    content = models.TextField()
+    rating = models.IntegerField()
--- cinema_critic_server/series/models.py
+++ cinema_critic_server/series/models.py
@@ -2,7 +2,7 @@ from django.core import validators
 from django.db import models
 
 # Create your models here.
-class Movie(models.Model):
+class Series(models.Model):
     name = models.CharField(
         max_length=30
     )
