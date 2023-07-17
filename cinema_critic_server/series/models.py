af109b24efb90b5dbf100c88412a4ccebeae0bd1
Victoria Stoyanova <your_github_email>
2023-07-17 15:20:23 +0300

    created Series model

--- cinema_critic_server/series/models.py
+++ cinema_critic_server/series/models.py
@@ -1,3 +1,34 @@
+from django.core import validators
 from django.db import models
 
 # Create your models here.
+class Movie(models.Model):
+    name = models.CharField(
+        max_length=30
+    )
+    year = models.IntegerField(
+        validators=[validators.MinValueValidator(1900)]
+    )
+    rating = models.IntegerField(
+        null=True,
+        blank=True,
+    )
+    director = models.CharField(
+        max_length=20)
+    stars = models.CharField(
+        max_length=50
+    )
+    visits = models.IntegerField(
+        blank=True,
+        null=True
+    )
+    genres = models.CharField(
+        max_length=30
+    )
+    trailer = models.URLField()
+    image = models.URLField()
+    length = models.CharField(
+        max_length=20
+    )
+    seasons = models.IntegerField()
+    episodes = models.IntegerField()
