940dd362e550e863f42e6b8db2886cadefbf4ea7
eftenow <mass4cre@icloud.com>
2023-07-18 15:44:07 +0300

    replace the MovieReview and SeriesReview with base model called Review, it uses a generic foreign key in order to bind to either the Movie or to the Series model, deppending on the occasion

--- cinema_critic_server/reviews/models.py
+++ cinema_critic_server/reviews/models.py
@@ -1,4 +1,6 @@
 from django.contrib.auth import get_user_model
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
 from django.db import models
 
 from cinema_critic_server.movies.models import Movie
@@ -7,25 +9,11 @@ from cinema_critic_server.series.models import Series
 UserModel = get_user_model()
 
 
-class MovieReview(models.Model):
+class Review(models.Model):
     review_title = models.CharField(max_length=30)
     user = models.ForeignKey(UserModel, on_delete=models.CASCADE)
-    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
-    content = models.TextField(
-        blank=True,
-        null=True
-    )
+    content = models.TextField(blank=True, null=True)
     rating = models.IntegerField()
-
-    # Add other fields for the movie review as needed
-
-
-class SeriesReview(models.Model):
-    review_title = models.CharField(max_length=30)
-    user = models.ForeignKey(UserModel, on_delete=models.CASCADE)
-    series = models.ForeignKey(Series, on_delete=models.CASCADE)
-    content = models.TextField(
-        blank=True,
-        null=True
-    )
-    rating = models.IntegerField()
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey('content_type', 'object_id')
