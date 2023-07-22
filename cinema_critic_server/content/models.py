4078610637b9f928c0e4363e10eb313894ffdc58
eftenow <mass4cre@icloud.com>
2023-07-22 10:41:47 +0300

    moive and series now inherit new model called "content" which includes all their common fields

--- cinema_critic_server/content/models.py
+++ cinema_critic_server/content/models.py
@@ -7,77 +7,28 @@ from cinema_critic_server.common.models import Genre
 
 
 # Create your models here.
-class Series(models.Model):
-    name = models.CharField(
-        max_length=30
-    )
-    year = models.IntegerField(
-        validators=[validators.MinValueValidator(1900)]
-    )
-    rating = models.IntegerField(
-        null=True,
-        blank=True,
-    )
-    director = models.CharField(
-        max_length=30)
-    stars = models.CharField(
-        max_length=60
-    )
-    visits = models.IntegerField(
-        blank=True,
-        null=True
-    )
-    slug = models.SlugField(
-        unique=True,
-        editable=False,
-        null=True
-    )
+class Content(models.Model):
+    name = models.CharField(max_length=30)
+    year = models.IntegerField(validators=[validators.MinValueValidator(1900)])
+    rating = models.IntegerField(null=True, blank=True)
+    director = models.CharField(max_length=30)
+    stars = models.CharField(max_length=60)
+    visits = models.IntegerField(blank=True, null=True)
+    slug = models.SlugField(unique=True, editable=False, null=True)
     genres = models.ManyToManyField(Genre)
     trailer = models.URLField()
     image = models.URLField()
-    length = models.CharField(
-        max_length=50
-    )
+    length = models.CharField(max_length=50)
+    created_at = models.DateTimeField(default=timezone.now, null=True, blank=True)
+
+    class Meta:
+        abstract = True
+
+
+class Movie(Content):
+    pass
+
+
+class Series(Content):
     seasons = models.IntegerField()
     episodes = models.IntegerField()
-    created_at = models.DateTimeField(
-        default=timezone.now,
-        null=True,
-        blank=True)
-
-
-class Movie(models.Model):
-    name = models.CharField(
-        max_length=30
-    )
-    year = models.IntegerField(
-        validators=[validators.MinValueValidator(1900)]
-    )
-    rating = models.IntegerField(
-        null=True,
-        blank=True,
-    )
-    director = models.CharField(
-        max_length=30)
-    stars = models.CharField(
-        max_length=60
-    )
-    visits = models.IntegerField(
-        blank=True,
-        null=True
-    )
-    slug = models.SlugField(
-        unique=True,
-        editable=False,
-        null=True
-    )
-    genres = models.ManyToManyField(Genre)
-    trailer = models.URLField()
-    image = models.URLField()
-    length = models.CharField(
-        max_length=50
-    )
-    created_at = models.DateTimeField(
-        default=timezone.now,
-        null=True,
-        blank=True)
