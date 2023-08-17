66bb3d86f9ef9be9145c8b643869190cb8203aaa
eftenow <mass4cre@icloud.com>
2023-08-17 09:19:43 +0300

    added custom ordering so that administrators appear first, then moderators, then everybody else in the admin panel, used annotation for this

--- cinema_critic_server/accounts/views_admin.py
+++ cinema_critic_server/accounts/views_admin.py
@@ -1,8 +1,6 @@
 from django.contrib.auth import get_user_model
-from django.contrib.auth.models import User
 from django.http import JsonResponse
-
-UserModel = get_user_model()
+from django.db.models import Case, When, Value, IntegerField
 
 
 def get_users(request):
@@ -11,20 +9,32 @@ def get_users(request):
 
     UserModel = get_user_model()
 
+    # custom ordering for user roles so that they appear sorted in the front-end
+    ordering = Case(
+        When(groups__name="Administrator", then=Value(1)),
+        When(groups__name="Moderator", then=Value(2)),
+        When(groups__name="Regular User", then=Value(3)),
+        default=Value(4),
+        output_field=IntegerField()
+    )
+
+    users = UserModel.objects.annotate(role_order=ordering).order_by('role_order', 'username')
+
     users_data = []
-    users = UserModel.objects.all()
     for user in users:
         role = user.groups.all().first()
         role_name = role.name if role else "No Role"
+
         user_data = {
             'username': user.username,
             'email': user.email,
             'role': role_name
         }
         users_data.append(user_data)
+
     """
         by specifying safe=False, we guarantee that we want to return a list
         this is used when returning lists/sets/tuples, and not dictionaries, which
         django typically expects 
-        """
+    """
     return JsonResponse(users_data, safe=False)
