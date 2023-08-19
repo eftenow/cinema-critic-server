7de5df98ee71936d067b20ebfc7dfd2a6793c839
eftenow <mass4cre@icloud.com>
2023-08-19 23:31:34 +0300

    switched from sqllite to postreSQL

--- cinema_critic_server/settings.py
+++ cinema_critic_server/settings.py
@@ -84,14 +84,18 @@ TEMPLATES = [
 WSGI_APPLICATION = 'cinema_critic_server.wsgi.application'
 
 # Database
-# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'cinema_critic_db',
+        'USER': 'postgres',
+        'PASSWORD': '12345',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
